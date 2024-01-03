f83cd1ad67b8e4695fd905ee80f1effe5f6bbed0
[김민식][기술개발본부][대리] <142783357+minsikkkkk@users.noreply.github.com>
2024-01-03 17:35:12 +0900

    포트폴리오 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,12 +1,13 @@
+export function menu() {
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
 
-const headerToggle = document.getElementById("headerToggle");
-const headerNav = document.querySelector(".header__nav");
-
-if(headerToggle){
-    headerToggle.addEventListener("click",() => {
-        headerNav.classList.toggle("show");
-        headerToggle.getAttribute("aria-expanded")==="true"
-        ?headerToggle.setAttribute("aria-expanded", "false")
-            :headerToggle.setAttribute("aria-expanded", "true");
-    });
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+            headerToggle.getAttribute("aria-expanded") === "true"
+                ? headerToggle.setAttribute("aria-expanded", "false")
+                : headerToggle.setAttribute("aria-expanded", "true");
+        });
+    }
 }
